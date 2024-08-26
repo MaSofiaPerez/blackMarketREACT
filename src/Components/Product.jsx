3a2392fa01403cd52ae672db1c1400b372fdf08c
antoniacasariego1 <101377493+antoniacasariego@users.noreply.github.com>
2024-08-25 22:36:58 -0400

    agregue boton para agregar al carrito desde home
    modifique addToCartButton para que cuando el usuario haga hover se oscurezca un poco el color

--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router-dom";
+import AddToCartButton from "./AddToCartButton";
 
 const Product = ({ product }) => {
     const navigate = useNavigate();
@@ -8,11 +9,16 @@ const Product = ({ product }) => {
     }
 
     return (
-        <div className="min-w-[200px] bg-white p-4 shadow-lg rounded" onClick = {handleClick}>
-            <img src={product.pictures[0]} alt={product.title} className="w-full h-40 object-cover rounded mb-2" />
-            <h4 className="text-lg font-bold">{product.title}</h4>
-            <p className="text-gray-600">{product.description}</p>
-            <p className="text-gray-600">{product.unit_price}</p>
+        <div className="min-w-[200px] bg-white p-4 shadow-lg rounded flex flex-col justify-between">
+            <div onClick={handleClick} className="cursor-pointer flex-grow">
+                <img src={product.pictures[0]} alt={product.title} className="w-full h-40 object-cover rounded mb-2" />
+                <h4 className="text-lg font-bold">{product.title}</h4>
+                <p className="text-gray-600">{product.description}</p>
+                <p className="text-gray-600">{product.unit_price}</p>
+            </div>
+            <div className="mb-4">
+                <AddToCartButton productId={product.id} />
+            </div>
         </div>
     );
 };
