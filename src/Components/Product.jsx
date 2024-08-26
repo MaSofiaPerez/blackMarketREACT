7573237d3f5dc4b02926d89006f0acafa289d273
antoniacasariego1 <101377493+antoniacasariego@users.noreply.github.com>
2024-08-25 23:21:31 -0400

    arreglar un poquito el estilo de cada producto

--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -9,14 +9,17 @@ const Product = ({ product }) => {
     }
 
     return (
-        <div className="min-w-[200px] bg-white p-4 shadow-lg rounded flex flex-col justify-between">
+        <div className="min-w-[200px] bg-white p-4 shadow-lg rounded flex flex-col 
+        justify-between transition-transform transform hover:scale-105 hover:shadow-xl">
             <div onClick={handleClick} className="cursor-pointer flex-grow">
-                <img src={product.pictures[0]} alt={product.title} className="w-full h-40 object-cover rounded mb-2" />
-                <h4 className="text-lg font-bold">{product.title}</h4>
-                <p className="text-gray-600">{product.description}</p>
-                <p className="text-gray-600">{product.unit_price}</p>
+                <img src={product.pictures[0]} alt={product.title} className="w-full h-40 object-cover rounded mb-4" />
+                <div className="flex flex-col flex-grow">
+                    <h4 className="text-lg font-bold">{product.title}</h4>
+                    <p className="text-lg text-gray-700 font-semibold mb-2">{product.unit_price}</p>
+                    <p className="text-sm text-gray-600">{product.description}</p>
+                </div>
             </div>
-            <div className="mb-4">
+            <div className="mb-4 flex justify-center">
                 <AddToCartButton productId={product.id} />
             </div>
         </div>
